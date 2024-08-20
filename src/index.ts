3c69a764fd92cfed89550df369b80f55465ad0e1
Sandy Agafonoff <sandy.agafonoff@gmail.com>
2024-08-20 19:36:38 +1000

    add types that represent the existing code

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,10 @@
 import { Config } from './providers/config';
-import { getPlaceAutocomplete } from './maps-api';
+import { getPlaceAutocomplete, PlaceSearchResult } from './maps-api';
 
-export async function getAutoCompleteDetails(address: any): Promise<any> {
+export async function getAutoCompleteDetails(address: any): Promise<PlaceSearchResult[]> {
   // get autocomplete results
   const res = getPlaceAutocomplete(Config.getTomTomApiKey(), address).then(
-    async (autocompleteResults) => {
-      const res: [] = [];
-      return res;
-    }
+    async (autocompleteResults) => autocompleteResults
   );
   // loop over and get details and map results
   return res;
--- src/maps-api.ts
+++ src/maps-api.ts
@@ -1,16 +1,31 @@
-import axios from 'axios'
+import axios, { AxiosResponse } from 'axios';
+
+export type PlaceSearchResult = {
+  placeId: string;
+};
+
+type TomTomAPIResult = {
+  id: string;
+};
+
+type TomTomAPIResponse = AxiosResponse<{
+  results: TomTomAPIResult[];
+}>;
 
 // https://developer.tomtom.com/search-api/documentation/search-service/fuzzy-search
 export async function getPlaceAutocomplete(key: string, address: string) {
-    const autocomplete = await axios.get(`https://api.tomtom.com/search/2/search/${address}.json'`, {
-        params: {
-            key,
-            limit: 100,
-        }
-    });
-    return autocomplete.data.results.map((result) => {
-        return {
-            placeId: result.id,
-        }
-    })
+  const autocomplete: TomTomAPIResponse = await axios.get(
+    `https://api.tomtom.com/search/2/search/${address}.json`,
+    {
+      params: {
+        key,
+        limit: 100
+      }
+    }
+  );
+  return autocomplete.data.results.map((result): PlaceSearchResult => {
+    return {
+      placeId: result.id
+    };
+  });
 }
